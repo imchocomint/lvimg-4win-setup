47be3110344b42fae14a730c3e40322e412aeef7
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2025-02-13 12:50:13 +0700

    making installing packages hassle-free

--- lvimg-inst-1.ps1
+++ lvimg-inst-1.ps1
@@ -1,4 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install git.install neovide.install make python nodejs-lts rust ripgrep luarocks winlibs neovim
-choco install powershell-core
+choco install git.install neovide.install make python nodejs-lts rust ripgrep luarocks winlibs neovim -y
+choco install powershell-core -y
 git clone https://github.com/imchocomint/lvimg-4win
